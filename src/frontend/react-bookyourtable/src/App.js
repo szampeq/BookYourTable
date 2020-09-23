431d54994bbc8c0f95d22f91f7e3122a34bbfa7a
szampeq <szampeq@gmail.com>
2020-09-23 13:12:10 +0200

    + Added React Framework

---
+++ src/frontend/react-bookyourtable/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Testowa Aplikacja Rezerwacji Stolików</h1>
+      </div>
+    );
+  }
+}
+
