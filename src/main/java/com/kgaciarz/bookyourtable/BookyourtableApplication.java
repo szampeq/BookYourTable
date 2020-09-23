ea24e5df116d49e0340ff885ed14b4c6a066493e
szampeq <szampeq@gmail.com>
2020-09-23 11:58:32 +0200

    Initial commit

---
+++ src/main/java/com/kgaciarz/bookyourtable/BookyourtableApplication.java
@@ -0,0 +1,13 @@
+package com.kgaciarz.bookyourtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyourtableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookyourtableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kgaciarz/bookyourtable/BookyourtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgaciarz.bookyourtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyourtableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
