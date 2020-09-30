9e31c534c0385f3f0fe66906ef15d7dbd98e7f6e
szampeq <szampeq@gmail.com>
2020-09-30 11:33:08 +0200

    + design progress, guest site

--- src/frontend/react-bookyourtable/src/App.js
+++ src/frontend/react-bookyourtable/src/App.js
@@ -12,36 +12,67 @@ import ReservationList from './reservations/ReservationList';
 import AddReservation from './reservations/AddReservation';
 
 import logo from'./img/logo.png';
+import usericon from './img/user-icon.png';
+import restauranticon from './img/restaurant-icon.png';
+import reservationicon from './img/reservation-icon.png';
 
 export default class App extends React.Component {
   render(){
     return(
       <div>
         <div class="logo">
-            <img  src={logo} alt="fireSpot" width="360" height="80"/>
-            <a href="localhost" class="navlink"> zaloguj </a>
-            <a href="localhost" class="navlink"> zarejestruj </a>
+            <img  src={logo} alt="fireSpot" width="270" height="60"/>
+            <a href="rejestruj" target="_blank" class="buttonnav"> nie masz konta? zarejestruj się!</a>
+            <a href="loguj" target="_blank" class="buttonnav">zaloguj się </a>
         </div>
 
         <div class="grid-container">
 
             <div class="info">
               <div class="infobackground">
-              <h1>Nowoczesny system rezerwacji stolików w restauracjach.</h1> <br></br><br></br>
+              <h1>Nowoczesny system rezerwacji stolików w restauracjach</h1> <br></br><br></br>
               Zarezerwuj stolik w jednej z Twoich ulubionych restauracji. <br></br>
               Szybko, przejrzyście, wygodnie. <br></br>
               Spróbuj, to nic nie kosztuje! Jesteśmy pewni, że zostaniesz z nami na dłużej! <br></br><br></br>
               <h4>Prowadzisz restaurację? Dołącz do nas!</h4>
               </div>
-
             </div>
 
             <div class="steps">
+            <div class="infosteps">
+              <h2>Rezerwacja stolika w 3 prostych krokach!</h2> <br></br><br></br><br></br><br></br>
 
+              <div class="rows">
+
+                  <div class="stepscontent">
+                    <img  src={usericon} alt="fireSpot"/>
+                  <h4>Rejestracja</h4> <br></br>
+                  Założenie konta pozwoli Ci na dokonywanie rezerwacji. Rejestracja jest szybka i intuicyjna.
+                  Warto poświęcić na nią te kilka minut.
+                  </div>
+
+                  <div class="stepscontent">
+                    <img  src={restauranticon} alt="fireSpot"/>
+                  <h4>Wybór restauracji</h4><br></br>
+                  Wybierz swoją ulubioną restaurację lub poszukaj czegoś nowego.
+                  Na pewno znajdziesz coś dla siebie!
+                  </div>
+
+                  <div class="stepscontent">
+                    <img  src={reservationicon} alt="fireSpot"/>
+                  <h4>Rezerwacja stolika</h4><br></br>
+                  Już prawie gotowe! Pozostało wybrać datę oraz stolik na odpowiednią ilość osób.
+                  To wszystko, smacznego!
+                  </div>
+
+                </div>
+
+              </div>
             </div>
 
             <div class="joinus">
-
+              <h2>Głodny? Nie czekaj, zapraszamy!</h2>
+              <a href="rejestruj" target="_blank" class="registerbutton">Rejestracja</a>
             </div>
 
         </div>
